9421558735692bc0f0cab21ea95f773fc6a559a3
Lego-RL <joeylbarroso@gmail.com>
2026-03-05 15:53:14 -0600

    reading data in

---
+++ src/validate.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def validate_data(file_path: str) -> pd.DataFrame:
+    """Return DataFrame storing validated dataset."""
+
+    df: pd.DataFrame = pd.read_csv(file_path)
+
+    # actually validate the data
+
+    return df
+
+
+# for testing
+if __name__ == "__main__":
+    print(validate_data("data/horror_movies.csv").head())
