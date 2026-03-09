c3b87c6b105f0880514da3c82a7cb85a91802394
Tenuuu <charlestenuta@gmail.com>
2026-03-09 12:59:27 -0500

    cleaned up analysis.py a bit

--- src/analysis.py
+++ src/analysis.py
@@ -9,13 +9,21 @@ def analyze_basic_data(df: pd.DataFrame) -> dict:
     """
     Analyze the provided DataFrame and return a dict. with very basic results.
 
-    Taking DF and giving a set of info.
+    Args:
+        df: DataFrame to be analyzed
+
+    Returns:
+        basic_data: Dict. containing basic analysis results
     """
     basic_data = {}
 
+    # retrieve basic data and append the key-value pairs to the basic_data dict.
     basic_data["total_movies"] = len(df)
-    basic_data["average_runtime"] = df["runtime"].mean()
-    basic_data["average_budget"] = df["budget"].mean()
-    basic_data["average_revenue"] = df["revenue"].mean()
+    basic_data["average_runtime"] = round(float(df["runtime"].mean()), 2)
+    basic_data["average_budget"] = round(float(df["budget"].mean()), 2)
+    basic_data["average_revenue"] = round(float(df["revenue"].mean()), 2)
 
     return basic_data
+
+# for testing purposes
+print(analyze_basic_data(load_data("../data/horror_movies.csv")))
