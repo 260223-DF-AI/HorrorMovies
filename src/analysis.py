c1891b34b57273f2c127aae0fa64e9ef165fee83
Tenuuu <charlestenuta@gmail.com>
2026-03-09 11:09:15 -0500

    start on analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -3,4 +3,14 @@ For analysis-related functionality
 """
 
 import pandas as pd
+from validate import load_data
 
+def analyze_data(df: pd.DataFrame) -> dict:
+    """
+    Analyze the provided DataFrame and return a dict. with the results.
+
+    Taking DF and giving a set of info.
+    """
+    analyzed = {}
+
+    return analyzed
