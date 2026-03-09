0c164aee400280b27c661a5f42d0d3ee93626eb8
Benjamin Ledoux <bledoux2002@gmail.com>
2026-03-09 09:53:05 -0500

    implemented validate_data(), based on the dataanalysis exercise

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 ## PROJECT 1
 
 ## Description
-
+Database Explorer, summarizing and analyzing a dataset on Horror Movies through terminal-based menus. Output of matplotlib graphs and charts to visualize data.
 
 ## Contributors
+- Joseph Barroso
 - Benjamin Ledoux
 - Charles Tenuta
-- Joseph Barroso
+
+## Resources
+- [Horror Movies Dataset](https://www.kaggle.com/datasets/sujaykapadnis/horror-movies-dataset)
--- src/validate.py
+++ src/validate.py
@@ -5,15 +5,61 @@ Functionality related to cleansing & validating our dataset
 """
 
 
-def validate_data(file_path: str) -> pd.DataFrame:
-    """Return DataFrame storing validated dataset."""
-
-    df: pd.DataFrame = pd.read_csv(file_path)
-
-    # actually validate the data
+def load_data(filepath: str) -> pd.DataFrame:
+    """
+    Load data from file, parse through and clean for analysis and manipulation
+    
+    Args:
+        filepath (str): Path to file to be opened, including file name
+    
+    Returns:
+        df (pd.DataFrame): Parsed and cleaned dataframe.
+    """
+    df = pd.read_csv(filepath)
+    df = validate_data(df)
 
     return df
 
+def validate_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Clean and validate provided DataFrame
+    - Drop duplicates
+    - Type checking
+    - Fill/Drop missing values
+    """
+    df.drop_duplicates()
+    # Drop unused columns
+    df.drop(columns=["poster_path", "status", "backdrop_path"])
+
+    # Try to fill missing title/original_title with original_title/title respectively
+    df["title"] = df["title"].fillna(df["original_title"])
+    df["original_title"] = df["original_title"].fillna(df["title"])
+
+    # Fill/Drop Missing Values
+    # If both original_title and title were missing, they will remain as NaN, drop
+    df = df.dropna(subset=["id", "original_title", "title"])
+
+    # Type Checking
+    # TODO: maybe drop type conversions (to_numeric, to_datetime, astype), only adding right now as confirmation read_csv properly converted everything
+    df["id"] = pd.to_numeric(df["id"])
+    df["release_date"] = pd.to_datetime(df["release_date"])
+    df["popularity"] = pd.to_numeric(df["popularity"])
+    df["vote_count"] = pd.to_numeric(df["vote_count"])
+    df["vote_average"] = pd.to_numeric(df["vote_average"])
+    df["budget"] = pd.to_numeric(df["budget"])
+    df["revenue"] = pd.to_numeric(df["revenue"])
+    df["runtime"] = pd.to_numeric(df["runtime"])
+    df["adult"] = df["adult"].astype("bool")
+    df["collection"] = pd.to_numeric(df["collection"])
+
+    # String standardization
+    df["original_title"] = df["original_title"].str.title().str.strip()
+    df["title"] = df["title"].str.title().str.strip()
+    df["original_language"] = df["original_language"].str.title().str.strip()
+    df["genre_names"] = df["genre_names"].str.title().str.strip()
+    df["collection_name"] = df["collection_name"].str.title().str.strip()
+
+    return df
 
 # for testing
 if __name__ == "__main__":
