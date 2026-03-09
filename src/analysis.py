c79ff4829e886e299ae4e248a93ac92ef0f932a1
Tenuuu <charlestenuta@gmail.com>
2026-03-09 11:14:40 -0500

    added analyze_basic_data()

--- src/analysis.py
+++ src/analysis.py
@@ -5,12 +5,17 @@ For analysis-related functionality
 import pandas as pd
 from validate import load_data
 
-def analyze_data(df: pd.DataFrame) -> dict:
+def analyze_basic_data(df: pd.DataFrame) -> dict:
     """
-    Analyze the provided DataFrame and return a dict. with the results.
+    Analyze the provided DataFrame and return a dict. with very basic results.
 
     Taking DF and giving a set of info.
     """
-    analyzed = {}
+    basic_data = {}
 
-    return analyzed
+    basic_data["total_movies"] = len(df)
+    basic_data["average_runtime"] = df["runtime"].mean()
+    basic_data["average_budget"] = df["budget"].mean()
+    basic_data["average_revenue"] = df["revenue"].mean()
+
+    return basic_data
