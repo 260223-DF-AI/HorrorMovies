5de743306d7c9b2f68042809d243d0e6c5c08d6b
Tenuuu <charlestenuta@gmail.com>
2026-03-09 08:40:44 -0500

    basic implementation for analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -1,5 +1,11 @@
-
-
 """
 For analysis-related functionality
 """
+
+import pandas as pd
+
+def read_data(file_path) -> pd.DataFrame:
+    """
+    Read data from a CSV file and return a DataFrame
+    """
+    return pd.read_csv(file_path)
