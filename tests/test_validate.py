7e062ef35bda44e5178e25c3fe0b7ea5241da494
Tenuuu <charlestenuta@gmail.com>
2026-03-11 14:09:12 -0500

    fixed test_load_data

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -7,23 +7,25 @@ import pytest
 
 from src.validate import load_data
 
-@pytest.mark.parametrize("filepath,row_expected,column_expected", [
+@pytest.mark.parametrize("filepath,column,row", [
     ("data/horror_movies.csv", 32540, 17),
     ("data/sample.json", 6, 4)
 ])
-def test_load_data(filepath, row_expected, column_expected):
-    """Test loading our horror_movies dataset"""
+def test_load_data(filepath, column, row):
+    """Test loading our files and validating the data"""
+    
     df = load_data(filepath)
     assert isinstance(df, pd.DataFrame)
     assert not df.empty
 
-    # ideal number of columns after validation
-    assert df.shape[1] == column_expected
-    assert df.shape[0] == row_expected
+    # ideal number of columns and rows after validation
+    assert df.shape[0] == column
+    assert df.shape[1] == row
 
-    # checks specific values of the DataFrame
-    assert df.iloc[1, 2] == 145
-    assert df.iloc[2, 1] == 103
+    # checks specific values of the JSON DataFrame
+    if filepath == 'data/sample.json':
+        assert df.iloc[1, 2] == 145
+        assert df.iloc[2, 1] == 103
 
 def test_nonexistent_file():
     """Test loading a file that does not exist"""
