341185a213e90db66a91c560cf3804bca4e67d1e
Benjamin Ledoux <bledoux2002@gmail.com>
2026-03-11 14:26:16 -0500

    db.py classes complete

--- src/db.py
+++ src/db.py
@@ -8,7 +8,6 @@ from dotenv import load_dotenv
 from datetime import datetime
 import os
 
-# TODO: Split primary and foreign keys, finances table (1:1), collections table (1:M)
 
 def setup():
     """
@@ -28,14 +27,19 @@ class Movie(Base):
     original_title: Mapped[str] = mapped_column(String(50), nullable=False)
     title: Mapped[str] = mapped_column(String(50), nullable=False)
     original_language: Mapped[str] = mapped_column(String(2), nullable=False)
+    metadata: Mapped["Metadata"] = mapped_column(ForeignKey("metadatas.id"))
     release_date: Mapped[datetime] = mapped_column(Date)
-    genres: Mapped[list["Genre"]] = relationship(back_populates="movie")
-    collection_id: Mapped[int] = mapped_column(Integer)
+    rating: Mapped["Rating"] = mapped_column(ForeignKey("ratings.id"))
+    finance: Mapped["Finance"] = mapped_column(ForeignKey("finances.id"))
+    genres: Mapped[list["Genre"]] = relationship("genres", back_populates="movies")
+    # collection_id: Mapped[int] = mapped_column(ForeignKey("collections.id"))
+    collection: Mapped["Collection"] = relationship("collections", back_populates="movies")
 
-class Metadatum(Base):
-    __tablename__ = "metadata"
+class Metadata(Base):
+    __tablename__ = "metadatas"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    movie_id: Mapped[int] = mapped_column(ForeignKey("movies.id"))
     overview: Mapped[str] = mapped_column(Text)
     tagline: Mapped[str] = mapped_column(Text)
     adult: Mapped[str] = mapped_column(Boolean)
@@ -44,6 +48,7 @@ class Rating(Base):
     __tablename__ = "ratings"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    movie_id: Mapped[int] = mapped_column(ForeignKey("movies.id"))
     popularity: Mapped[float] = mapped_column(Float)
     vote_count: Mapped[int] = mapped_column(Integer)
     vote_average: Mapped[float] = mapped_column(Float)
@@ -52,13 +57,17 @@ class Finance(Base):
     __tablename__ = "finances"
 
     id: Mapped[int] = mapped_column()
+    movie_id: Mapped[int] = mapped_column(ForeignKey("movies.id"))
+    budget: Mapped[int] = mapped_column(Integer)
+    revenue: Mapped[int] = mapped_column(Integer)
 
 class Genre(Base):
     __tablename__ = "genres"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     genre_name: Mapped[str] = mapped_column(String(14), nullable=False)
-    movies: Mapped[list["Movie"]] = relationship(back_populates="genres")
+
+    movies: Mapped[list["Movie"]] = relationship("movies", back_populates="genres")
 
 class Movie_Genre(Base):
     # Junction Table
@@ -68,10 +77,14 @@ class Movie_Genre(Base):
     movie_id: Mapped[int] = mapped_column(ForeignKey("movies.id"))
     genre_id: Mapped[int] = mapped_column(ForeignKey("genres.id"))
 
-    # ???
+    # ??? do we need relationshps defined here? do we even need this class?
 
-class collection 
+class Collection(Base):
+    __tablename__ = "collections"
 
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    collection_name: Mapped[str] = mapped_column(String(30), nullable=False)
+    movies: Mapped[list["Movie"]] = relationship("movies", back_populates="collections")
 
 
 if __name__ == "__main__":
